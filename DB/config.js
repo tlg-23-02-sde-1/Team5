d5f5e8acdea364f18699af11581118eefcd8d4dd
Jorge Aponte <jorgealyaponte@yahoo.com>
2023-04-11 09:56:56 -0700

    Auth0 is implemented

--- DB/config.js
+++ DB/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect(process.env.DB_URL);
+    await mongoose.connect(process.env.DBURL);
     console.log("Established connection with the DB");
   } catch (error) {
     console.log(error);
--- server.js
+++ server.js
@@ -4,8 +4,24 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const { connect } = require("./DB/config");
 const { Router } = require("./Routes/routes");
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUERBASEURL
+};
 
 connect();
+
+app.set("view engine", "ejs");
+
+app.use(auth(config));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
@@ -16,7 +32,3 @@ app.use("/", Router);
 app.listen(process.env.PORT, () => {
   console.log(`Port Connected at ${process.env.PORT}`);
 });
-
-//         app.set('view engine','ejs')
-//         app.use(bodyParser.urlencoded({ extended: true }))
-//         app.use(bodyParser.json())
